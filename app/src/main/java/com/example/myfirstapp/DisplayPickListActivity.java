81263cc0ebfbc2af1bcab173e455d7fe99523dd7
Illendula, Venkata Sai Manoj <venkata.illendula@dematic.com>
2019-06-15 22:29:00 -0400

    Login screen initial items

--- app/src/main/java/com/example/myfirstapp/DisplayPickListActivity.java
+++ app/src/main/java/com/example/myfirstapp/DisplayPickListActivity.java
@@ -2,8 +2,7 @@ package com.example.myfirstapp;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.myfirstapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.InjectView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button _loginButton = (Button) findViewById(R.id.button);
+
+        _loginButton.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent inent = new Intent(this, LoginActivity.class);
+
+        startActivity(inent);
     }
 }
