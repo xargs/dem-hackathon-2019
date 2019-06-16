dd2f988a59a51de38d191419195f04bd9e0354ed
Illendula, Venkata Sai Manoj <venkata.illendula@dematic.com>
2019-06-16 14:12:25 -0400

    dependency on butterknife resolved - again

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -6,8 +6,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import butterknife.InjectView;
-
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
