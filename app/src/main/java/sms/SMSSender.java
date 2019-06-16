e02a667059cd64324c4120a008a1ceca83ac3dd0
Kaimal, Ravi K <ravi.kaimal@dematic.com>
2019-06-16 16:25:04 -0500

    Utility class for sending SMS

--- app/src/main/java/sms/SMSSender.java
+++ app/src/main/java/sms/SMSSender.java
@@ -8,7 +8,7 @@ import android.telephony.SmsManager;
 
 public class SMSSender {
     public static void sendSMS(Context context,String phoneNumber, String message){
-        if (ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_CALENDAR)
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.SEND_SMS)
                 != PackageManager.PERMISSION_GRANTED) {
             SmsManager smsManager = SmsManager.getDefault();
             smsManager.sendTextMessage(phoneNumber, null, message, null, null);
