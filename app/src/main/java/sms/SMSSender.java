f48cbc161b7012febf7436c646b88bf4cd5b4fae
Kaimal, Ravi K <ravi.kaimal@dematic.com>
2019-06-16 17:21:49 -0500

    Utility class for sending SMS

--- app/src/main/java/sms/SMSSender.java
+++ app/src/main/java/sms/SMSSender.java
@@ -1,17 +1,49 @@
 package sms;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.telephony.SmsManager;
 
 public class SMSSender {
-    public static void sendSMS(Context context,String phoneNumber, String message){
+
+
+    public static final int MY_PERMISSIONS_SEND_SMS = 1;
+
+    public static void sendSMS(Context context, String phoneNumber, String message){
+        if (ContextCompat.checkSelfPermission(context,
+                Manifest.permission.SEND_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale((Activity) context,
+                    Manifest.permission.SEND_SMS)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions((Activity)context,
+                        new String[]{Manifest.permission.SEND_SMS}, MY_PERMISSIONS_SEND_SMS);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        } else {
+            sendSMS(phoneNumber,message);
+        }
         if (ContextCompat.checkSelfPermission(context, Manifest.permission.SEND_SMS)
                 == PackageManager.PERMISSION_GRANTED) {
-            SmsManager smsManager = SmsManager.getDefault();
-            smsManager.sendTextMessage(phoneNumber, null, message, null, null);
         }
+    }
+
+    public static void sendSMS(String phoneNumber, String message){
+        SmsManager smsManager = SmsManager.getDefault();
+        smsManager.sendTextMessage(phoneNumber, null, message, null, null);
     }
 }
