69dfdd2d8e14e86fb46ec161ff105d3c009ac7b1
Parasam, Kaushik N <kaushik.parasam@dematic.com>
2019-06-17 10:37:04 -0500

    KP - Using full url for end point - 'http://us7813pc.amcs.tld'.

--- app/src/main/java/json/JsonConstants.java
+++ app/src/main/java/json/JsonConstants.java
@@ -3,7 +3,7 @@ package json;
 public class JsonConstants {
 
     /** Web service end points */
-    public static final String PTG_END_POINT                            = "http://us7813pc:8080/mgtp/resources/v1.0/ptg";
+    public static final String PTG_END_POINT                            = "http://us7813pc.amcs.tld:8080/mgtp/resources/v1.0/ptg";
     public static final String REGISTER_URL                             = "/register";
     public static final String DEREGISTER_URL                           = "/deregister";
     public static final String CONFIGURATION_REQUEST_URL                = "/configurationRequest";
@@ -13,7 +13,7 @@ public class JsonConstants {
     public static final String CONFIRM_PICK_URL                         = "/confirmPick";
     public static final String PICK_WALK_FINISH_URL                     = "/pickWalkFinish";
     public static final String PICK_CONTAINER_CONFIRMATION_URL          = "/pickContainerConfirmation";
-    public static final String SKU_IMAGES_URL                           = "http://us7813pc:8080/mgtp/resources/sku-images";
+    public static final String SKU_IMAGES_URL                           = "http://us7813pc.amcs.tld:8080/mgtp/resources/sku-images";
     public static final String SKU_IMAGE_EXTENSION                      = ".JPG";
 
     public static final String HACKER_10                                = "Hacker10";
